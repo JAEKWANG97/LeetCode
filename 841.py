0c71cbcdde747ea143d6091067cb9e37cbdfc413
Picka <marmong@naver.com>
2023-10-23 16:36:41 +0900

    iter BFS

---
+++ 841.py
@@ -0,0 +1,17 @@
+rooms =[[1,3],[3,0,1],[2],[0]]
+from collections import deque
+
+n = len(rooms)
+seen = deque()
+num =0 
+visited = [False for i in range(n)]
+visited[0]  = True
+seen.append(0)
+while seen:
+    neighborhood= seen.popleft()
+    if rooms[num]:
+        for x in rooms[num]:
+            if visited[x] == False and x not in seen:
+                seen.append(x)
+                visited[x] = True
+print(all(visited))
